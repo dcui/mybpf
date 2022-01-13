497e0254d4b84fdd4700dd022fb68be7faabe1c4
Dexuan Cui <decui@microsoft.com>
2022-01-13 02:48:04 +0000

    3

--- my.c
+++ my.c
@@ -1,13 +1,20 @@
 #!/usr/local/bin/bpftrace
 
 #include <net/sock.h>
+#include <net/inet_sock.h>
 
 kprobe:tcp_v4_connect
 / pid == 5492 /
 {
         $sk = (struct sock *)arg0;
 	$uaddr = (struct sockaddr *)arg1;
+	$usin = (struct sockaddr_in *)arg1;
 	$addr_len = arg2;
 
-	printf("sk=%p, uaddr=%p, addr_len=%d\n", $sk, $uaddr, $addr_len);
+	$inet = (struct inet_sock *)$sk;
+	$nexthop = $usin->sin_addr.s_addr;
+
+	printf("sk=%p, addr_len=%d, %s:[0x%x]->%s:[0x%x]\n", $sk, $addr_len,
+			ntop($inet->inet_saddr), $inet->inet_sport,
+			ntop(AF_INET, $nexthop), $usin->sin_port);
 }
